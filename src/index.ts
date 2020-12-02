f86d60c6f3b2e6ab6953cdd332d25017363b88e4
Yakir <got.yakir@gmail.com>
2020-12-03 00:39:22 +0100

    feat: connect weather api to weather details

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles.css";
 import { WeatherForm } from "./weather-form/weather-form";
 import { WeatherDetails } from "./weather-details/weather-details";
-import { WeatherDetailsModel } from "./weather-details/weather-details.model";
+import { CityCountryWeatherRequestData } from "./models/city-country-weather-request-data";
 
-new WeatherForm();
+function initWeatherMap(): void {
+  new WeatherForm();
+  const weatherDetails = new WeatherDetails();
 
-document.addEventListener("weatherFormSubmit", console.log);
+  document.addEventListener(
+    "weatherFormSubmit",
+    (event: CustomEvent<CityCountryWeatherRequestData>) =>
+      weatherDetails.updateWeatherDetails(event)
+  );
+}
 
-const weatherDetails = new WeatherDetails();
-
-const model = new WeatherDetailsModel();
-model.headerText = "Clear in Berlin, Germany";
-model.headerImageUrl = "http://openweathermap.org/img/wn/10d@2x.png";
-model.description = "clear sky";
-model.temperatureInCelsius = 2;
-
-weatherDetails.updateWeatherDetails(model);
+initWeatherMap();
--- src/weather-details/weather-details.ts
+++ src/weather-details/weather-details.ts
@@ -1,10 +1,15 @@
 import { WeatherDetailsModel } from "./weather-details.model";
+import { CityCountryWeatherRequestData } from "../models/city-country-weather-request-data";
+import { WeatherResponse } from "../models/weather-response";
+import { WeatherApi } from "../services/weather-api";
 
 export class WeatherDetails {
+  private readonly weatherApi: WeatherApi;
   private readonly weatherDetailsTemplate: HTMLTemplateElement | null;
   private readonly weatherDetailsContainer: HTMLDivElement | null;
 
   constructor() {
+    this.weatherApi = new WeatherApi();
     this.weatherDetailsTemplate = document.querySelector(
       "#weather-details-template"
     );
@@ -13,24 +18,47 @@ export class WeatherDetails {
     );
   }
 
-  updateWeatherDetails(weatherDetails: WeatherDetailsModel): void {
-    if (!this.weatherDetailsTemplate || !this.weatherDetailsContainer) {
-      return;
-    }
+  updateWeatherDetails(
+    event: CustomEvent<CityCountryWeatherRequestData>
+  ): void {
+    void this.weatherApi
+      .getWeatherByCityAndCountry(event.detail)
+      .then((weatherResponse: WeatherResponse) => {
+        const headerText = weatherResponse.weather[0].main;
+        const headerImageUrl = weatherResponse.weather[0].icon;
+        const description = weatherResponse.weather[0].description;
+        const temperatureInCelsius = Math.round(weatherResponse.main.temp);
 
-    const firstElementChild: Element | null = this.weatherDetailsTemplate
-      .content.firstElementChild;
+        const model = new WeatherDetailsModel();
+        model.headerText = `${headerText} at ${weatherResponse.name}`;
+        if (headerImageUrl) {
+          model.headerImageUrl = `http://openweathermap.org/img/wn/${headerImageUrl}@2x.png`;
+        }
+        model.description = description;
+        model.temperatureInCelsius = temperatureInCelsius;
 
-    if (firstElementChild) {
-      this.populateDetailsElementWithDetails(weatherDetails, firstElementChild);
+        if (!this.weatherDetailsTemplate || !this.weatherDetailsContainer) {
+          return;
+        }
 
-      const clone: Node = firstElementChild.cloneNode(true);
+        const firstElementChild: Element | null = this.weatherDetailsTemplate
+          .content.firstElementChild;
 
-      this.weatherDetailsContainer.appendChild(clone);
-    }
+        if (firstElementChild) {
+          this.weatherDetailsContainer.innerHTML = "";
+          WeatherDetails.populateDetailsElementWithDetails(
+            model,
+            firstElementChild
+          );
+
+          const clone: Node = firstElementChild.cloneNode(true);
+
+          this.weatherDetailsContainer.appendChild(clone);
+        }
+      });
   }
 
-  private populateDetailsElementWithDetails(
+  private static populateDetailsElementWithDetails(
     weatherDetails: WeatherDetailsModel,
     element: Element
   ): void {
